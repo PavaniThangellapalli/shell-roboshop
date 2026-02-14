375844ea491d49c384e5d7b78bc5438f30cc55e0
Pavani T <pavanit.0810@gmail.com>
2026-02-14 12:35:23 +0530

    Updating Catalogue in Shell

--- catalogue.sh
+++ catalogue.sh
@@ -4,6 +4,11 @@ app_name=catalogue
 
 NODEJS
 
+echo Allowing port
+firewall-cmd --permanent --add-port=8080/tcp &>>log_file
+firewall-cmd --reload &>>log_file
+Status_Print $?
+
 echo Copying MongoDB repo file
 cp $dir_path/mongo.repo /etc/yum.repos.d/mongo.repo &>>log_file
 Status_Print $?
